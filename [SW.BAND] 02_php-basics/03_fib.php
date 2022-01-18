1b6f20c7593b8d72d41721893feb347d16f62b94
stupid-laborant <iamawhale@mail.ru>
2022-01-18 11:13:51 +0100

    task 3 completed

--- [SW.BAND] 02_php-basics/03_fib.php
+++ [SW.BAND] 02_php-basics/03_fib.php
@@ -2,18 +2,22 @@
 echo "fib(0) = ".fib(0).'<br>';
 echo "fib(1) = ".fib(1).'<br>';
 echo "fib(10) = ".fib(10).'<br>';
-echo "fib(100) = ".fib(100).'<br>';
-echo "fib(999) = ".fib(100).'<br>';
+echo "fib(15) = ".fib(15).'<br>';
+echo "fib(21) = ".fib(21).'<br>';
 
-function fib($n, $arr) {
-    if (!isset($arr)) {
-        $arr= [0 => 0, 1=>1];
-    }
-    if (in_array($n, $arr)) {
-        return $arr[$n];
+function fib($n) {
+    if ($n == 0) {
+        return 0;
+    } elseif ($n == 1) {
+        return 1;
     } else {
-        $f_number = fib($n - 1, &$arr) + fib(n - 2, &$arr);
-        $storage[$n] = $f_number;
-        return $f_number;
+        $beforePrevious=0;
+        $previous=1;
+        for ($i=2; $i<=$n; $i++) {
+            $fib = $beforePrevious +$previous;
+            $beforePrevious = $previous;
+            $previous = $fib;
+        }
+        return $fib;
     }
 }
