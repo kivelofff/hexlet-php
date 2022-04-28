20f95950d434073ecf6ea3da95e1aa48db1fd3e4
user <ivan.elantyev@yandex.ru>
2022-04-28 12:56:48 +0200

    14 not completed

--- [SW.BAND] 10_php-functions/14_enlrageArrayImage.php
+++ [SW.BAND] 10_php-functions/14_enlrageArrayImage.php
@@ -52,3 +52,16 @@ enlargeArrayImage($image);
 используйте функции из библиотеки Funct
 
  */
+require_once "../vendor/autoload.php";
+
+use Funct\Collection;
+
+
+function enlargeArrayImage(array $imageArray): array
+{
+    $zipImage = [];
+
+    return $zipImage;
+}
+
+var_dump(Collection\zip([1,2,3,4,5], [1,2,3,4,5]));
