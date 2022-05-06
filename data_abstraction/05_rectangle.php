d736009ad9f3d98b3757d4169f79dc830f8c9345
ladmin <ladmin@ladmins.ky>
2022-05-06 03:01:10 +0200

    5 data abstractions doesn't work

---
+++ data_abstraction/05_rectangle.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Реализуйте абстракцию (набор функций) для работы с прямоугольниками, стороны которого всегда параллельны осям. Прямоугольник может располагаться в любом месте координатной плоскости.
+
+При такой постановке, достаточно знать только три параметра для однозначного задания прямоугольника на плоскости: координаты левой-верхней точки, ширину и высоту. Зная их, мы всегда можем построить прямоугольник одним единственным способом.
+
+|
+
+4 |    точка   ширина
+
+|       *-------------
+
+3 |       |            |
+
+|       |            | высота
+
+2 |       |            |
+
+|       --------------
+
+1 |
+
+|
+
+------|---------------------------
+
+0 |  1   2   3   4   5   6   7
+
+|
+
+|
+
+|
+
+Основной интерфейс:
+
+makeRectangle() (конструктор) – создает прямоугольник. Принимает параметры: левую-верхнюю точку, ширину и высоту. Ширина и высота – положительные числа.
+
+Селекторы getStartPoint(), getWidth() и getHeight()
+
+containsOrigin() – проверяет, принадлежит ли центр координат прямоугольнику (не лежит на границе прямоугольника, а находится внутри). Чтобы в этом убедиться, достаточно проверить, что все вершины прямоугольника лежат в разных квадрантах (их можно высчитать в момент проверки).
+
+<?php
+
+
+
+// Создание прямоугольника:
+
+// p - левая верхняя точка
+
+// 4 - ширина
+
+// 5 - высота
+
+//
+
+// p    4
+
+// -----------
+
+// |         |
+
+// |         | 5
+
+// |         |
+
+// -----------
+
+
+
+$p = makeDecartPoint(0, 1);
+
+$rectangle = makeRectangle($p, 4, 5);
+
+
+
+containsOrigin($rectangle); // false
+
+
+
+$rectangle2 = makeRectangle(makeDecartPoint(-4, 3), 5, 4);
+
+containsOrigin($rectangle2); // true
+
+Подсказки
+
+Квадрант плоскости — любая из 4 областей (углов), на которые плоскость делится двумя взаимно перпендикулярными прямыми, принятыми в качестве осей координат.
+
+ */
+namespace Dat\Rectangle;
+
+use function Dat\Implementations\Points\makeDecartPoint;
+use function Dat\Implementations\Points\getX;
+use function Dat\Implementations\Points\getY;
+use function Dat\Implementations\Points\getQuadrant;
+
+// BEGIN (write your solution here)
+function makeRectangle(array $startPoint, int $width, int $height): array
+{
+    return [
+        'startPoint' => $startPoint,
+        'width' => $width,
+        'height' => $height
+    ];
+}
+
+function getStartPoint(array $rectangle): array
+{
+    return $rectangle['startPoint'];
+}
+
+function getWidth(array $rectangle): int
+{
+    return $rectangle['width'];
+}
+
+function getHeight(array $rectangle): int
+{
+    return $rectangle['height'];
+}
+
+function containsOrigin(array $rectangle): bool
+{
+    $startPoint = getStartPoint($rectangle);
+    $width = getWidth($rectangle);
+    $height = getHeight($rectangle);
+
+    $firstQuadrant = getQuadrant($startPoint);
+
+    $secondPoint = makeDecartPoint(getX($startPoint) + $width, getY($startPoint));
+    $secondQuadrant = getQuadrant($secondPoint);
+
+    $thirdPoint = makeDecartPoint(getX($startPoint), getY($startPoint) - $height);
+    $thirdQuadrant = getQuadrant($thirdPoint);
+
+    $fourthPoint = makeDecartPoint(getX($startPoint) + $width, getY($startPoint) - $height);
+    $fourthQuadrant = getQuadrant($fourthPoint);
+
+    $quadrants = [$firstQuadrant, $secondQuadrant, $thirdQuadrant, $fourthQuadrant];
+    return count($quadrants) === count(array_unique($quadrants));
+}
+// END
+
+$rectangleOne = makeRectangle(makeDecartPoint(-1, 1), 2, 2);
+
+var_dump(containsOrigin($rectangleOne));
